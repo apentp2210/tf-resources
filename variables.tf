3f8c088bc76529952e71ebd4b5c583edfd65ca8c
An <an.trannhat@gmail.com>
2021-07-23 09:00:14 +0700

    add variable

--- variables.tf
+++ variables.tf
@@ -2,6 +2,9 @@ variable "main_region" {
   default = "us-east-1"
 }
 
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+
 variable "database_master_password" {
   default = "^H#>S$c8"
 }
